dfebcea360ad2b5168118c6db66ea2f7bd1cf114
YuanWeiChen16 <46083885+YuanWeiChen16@users.noreply.github.com>
2020-05-21 14:26:23 +0800

    init 2

---
+++ New Unity Project (1)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject[] originOBJ;
+    public GameObject NewGameOBJ;
+    public int Number;
+    public Vector3 spawnPos = new Vector3(-2f, 1.86f, 2f);
+
+    public GameObject textt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NewGameOBJ==null)
+        {
+            int tempp= Random.Range(0,4);
+            NewGameOBJ = Instantiate(originOBJ[tempp], spawnPos, Quaternion.identity);
+        }
+
+        Vector3 temp = NewGameOBJ.GetComponent<Transform>().position;
+        temp.z -= 0.5f*Time.deltaTime;
+        NewGameOBJ.GetComponent<Transform>().position = temp;
+        if(temp.z<-2)
+        {
+           
+            Destroy(NewGameOBJ);
+            int tempp = Random.Range(0, 4);
+            NewGameOBJ = Instantiate(originOBJ[tempp], spawnPos, Quaternion.identity);
+        }
+    }
+
+    public void CallThisScript()
+    {
+        
+        Number++;
+        Debug.Log("call game controller"+ Number);
+
+        textt.GetComponent<Text>().text = "HIT : " + Number;
+    }
+}
---
+++ New Unity Project (1)/Assets/cube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cube : MonoBehaviour
+{
+
+    NewBehaviourScript gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<NewBehaviourScript>();
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameController.CallThisScript();
+        //Debug.Log("DIE : ");
+    }
+}
