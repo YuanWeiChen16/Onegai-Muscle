f0bb4db4b8de1093e333c9795a0944d6535bddd4
YuanWeiChen16 <46083885+YuanWeiChen16@users.noreply.github.com>
2020-06-10 21:23:39 +0800

    update 06102123

---
+++ New Unity Project (1)/Assets/OurGameObj/Script/NewBehaviourScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject[] originOBJ;
+    public GameObject NewGameOBJ;
+    public int Number;
+    public Vector3 spawnPos;
+
+    public GameObject textt;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NewGameOBJ == null)
+        {
+            int tempp = Random.Range(0, 4);
+            NewGameOBJ = Instantiate(originOBJ[tempp], spawnPos, Quaternion.identity);
+            Debug.Log(tempp);
+        }
+
+        Vector3 temp = NewGameOBJ.GetComponent<Transform>().position;
+        temp.z -=Time.deltaTime;
+        NewGameOBJ.GetComponent<Transform>().position = temp;
+        if (temp.z < -1)
+        {
+            Destroy(NewGameOBJ);
+            int tempp = Random.Range(0, 4);
+
+            NewGameOBJ = Instantiate(originOBJ[tempp], spawnPos, Quaternion.identity);
+            Debug.Log(tempp);
+        }
+    }
+
+    public void CallThisScript()
+    {
+
+        Number++;
+        Debug.Log("call game controller" + Number);
+
+        textt.GetComponent<Text>().text = "HIT : " + Number;
+    }
+}
---
+++ New Unity Project (1)/Assets/OurGameObj/Script/cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cube : MonoBehaviour
+{
+
+    NewBehaviourScript gameController;
+    public bool hited = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<NewBehaviourScript>();
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hited == false)
+        {
+            gameController.CallThisScript();
+            hited = true;
+        }
+        Debug.Log("HIT!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
